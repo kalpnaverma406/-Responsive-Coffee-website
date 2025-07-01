514ffab9d6cb58a1b131b3dcf2af7a9788c1dd4c
Kalpna verma <vermakalpna714@gmail.com>
2025-06-30 23:22:10 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+const navlinks= document.querySelectorAll(".nav-menu .nav-link")
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menucloseButton = document.querySelector("#menu-close-button");
+menuOpenButton.addEventListener("click",()=>{
+    // toggle mobile-menu visibility
+    document.body.classList.toggle
+    ("show-mobile-menu");
+});
+//close menu when the close button is clicked
+ menucloseButton.addEventListener("click",()=>menuOpenButton.click());
+ //close menu when nav link is clicked
+navlinks.forEach(link=>{
+  link.addEventListener("click",()=> menuOpenButton.click());
+});
+ 
+//Initialize Swiper
+ const swiper = new Swiper('.slider-wrapper', {
+  grabcursor:true,
+
+  loop: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets:true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }, 
+  //responsive breakpoints
+  breakpoints:{
+    0:{
+        slidesPerView: 1
+    },
+     768:{
+        slidesPerView: 2
+    },
+
+     1024:{
+        slidesPerView: 3
+    }
+  }
+});
